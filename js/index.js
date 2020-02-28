f2472b45eca2c1bfbd6dc9b30feec7f96f15bd7a
Rogelio <rogercp1234@yahoo.com>
2020-02-28 14:34:36 -0800

    added smoothscroll

--- js/index.js
+++ js/index.js
@@ -1,8 +1,21 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+  
+
+
 function onload() {
     populateHTML();
   
   }
   
 const populateHTML = () =>{
-    
+
     }
+
