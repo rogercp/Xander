d2abf156d62db9d0e3d6df9b2c7af4b23098d0d1
Rogelio <rogercp1234@yahoo.com>
2020-02-28 15:44:40 -0800

    populated some data

--- data/data.js
+++ data/data.js
@@ -1,3 +1,18 @@
 let profile = {
 
+name: "Xander Bennett",
+resume:"Resume",
+description: "Data Scientist | Machine Learning Engineer",
+bio:"I’m Xander. I’m a data scientist and machine learning engineer with a finance background, and I think that data science is the key to obtaining a winning formula. My interests are all over the place, and data science can be used to make tidy conclusions about all of them and more.",
+email: "zanderwhiteleybennett@gmail.com",
+location: "San Diego, California",
+
+social: {
+resume:"./data/Resume.pdf",
+linkedin: "",
+twitter: "",
+github: "",
+dev: ""
+},
+
 }
--- js/index.js
+++ js/index.js
@@ -16,6 +16,50 @@ function onload() {
   }
   
 const populateHTML = () =>{
+// Profile image
+if (profile.picture != "") {
+    $("#profileImg").attr("src", profile.picture);
+  }
+  $("#profileImg").attr("alt", profile.name);
+  // About
+  $("#smallName").text(profile.name);
+  $("#name")
+    .find(".first")
+    .text(profile.name.split(" ")[0]);
+  $("#name")
+    .find(".second")
+    .text(
+      profile.name
+        .split(" ")
+        .slice(1)
+        .join(" ")
+    );
+  document.getElementById("location").innerHTML = profile.location;
+ 
+  $("#email")
+    .find(".email")
+    .text(profile.email);
+  document
+    .getElementById("email")
+    .setAttribute("href", "mailto:" + profile.email);
+  document
+  .getElementById("resume").setAttribute("href", profile.social.resume)
+  $(".resume").text(profile.resume);
+  $("#description")
+    .find(".quote")
+    // .text(`"${profile.quote}"`);
+  $("#description")
+    .find(".description")
+    .text(profile.description);
+  
+  // Social
+  document.getElementById("linkedin").setAttribute("href", profile.social.linkedin);
+  document.getElementById("github").setAttribute("href", profile.social.github);
+  document.getElementById("twitter").setAttribute("href", profile.social.twitter);
+  document.getElementById("medium").setAttribute("href", profile.social.medium);
+  document.getElementById("bio-text").innerHTML = profile.bio;
+
+
 
     }
 
