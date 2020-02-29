9322eebd519796a4046c091746da0035191c1085
Rogelio <rogercp1234@yahoo.com>
2020-02-28 16:12:37 -0800

    more styling to Profile

--- js/index.js
+++ js/index.js
@@ -7,8 +7,15 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
-  
 
+$(function(){ 
+    var navMain = $(".navbar-collapse"); // avoid dependency on #id
+    // "a:not([data-toggle])" - to avoid issues caused
+    // when you have dropdown inside navbar
+    navMain.on("click", "a:not([data-toggle])", null, function () {
+        navMain.collapse('hide');
+    });
+});
 
 function onload() {
     populateHTML();
